97c1fdc4b830387fba0b7469ad56564ea4e0b3b9
nikitahasert@gmail.com <nikitahasert@gmail.com>
2017-09-02 15:11:56 +0200

    Add class for loading ROMs

---
+++ src/de/nihas101/chip8/utils/RomLoader.java
@@ -0,0 +1,65 @@
+package de.nihas101.chip8.utils;
+
+import de.nihas101.chip8.hardware.memory.Chip8Memory;
+import de.nihas101.chip8.unsignedDataTypes.UnsignedByte;
+
+import java.io.*;
+
+import static de.nihas101.chip8.utils.Constants.MEMORY_LENGTH;
+import static de.nihas101.chip8.utils.Constants.PROGRAM_COUNTER_START;
+
+/**
+ * Loads ROMs from a hard drive
+ */
+public class RomLoader {
+    /**
+     * Loads a ROM into memory
+     * @param romPath The path to the ROM
+     * @param memory The memory to load the ROM into
+     */
+    public void loadRom(String romPath, Chip8Memory memory) {
+        File romFile = new File(romPath);
+        InputStream inputStream = null;
+        byte[] loadedRom = new byte[MEMORY_LENGTH - PROGRAM_COUNTER_START];
+        int readBytes = -1;
+
+        try {
+            inputStream = new FileInputStream(romFile);
+            readBytes = inputStream.read(loadedRom);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            closeFileStream(inputStream);
+        }
+
+        if(readBytes > 0) loadIntoMemory(memory, loadedRom);
+        else System.out.println("No bytes were read");
+    }
+
+    /**
+     * Closes the {@link InputStream}
+     * @param inputStream The {@link InputStream} to close
+     */
+    private void closeFileStream(InputStream inputStream){
+        if (inputStream != null) {
+            try {
+                inputStream.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Loads a ROM into memory
+     * @param memory The memory to load the rom into
+     * @param loadedRom The ROM to load
+     */
+    private void loadIntoMemory(Chip8Memory memory, byte[] loadedRom){
+        int MAX_ROM_LENGTH = MEMORY_LENGTH - PROGRAM_COUNTER_START;
+        for(int i = 0 ; i < MAX_ROM_LENGTH ; i++){
+            memory.write(PROGRAM_COUNTER_START+i, new UnsignedByte(loadedRom[i]));
+        }
+    }
+}
