a1b116ca6a06581402e02ff74fbf8bf88229979a
Nikita Hasert <nikitahasert@gmail.com>
2018-01-26 20:26:25 +0100

    Add SaveStateHandler class

---
+++ src/main/java/de/nihas101/chip8/savestates/SaveStateHandler.java
@@ -0,0 +1,47 @@
+package de.nihas101.chip8.savestates;
+
+import java.io.*;
+import java.util.logging.Logger;
+
+public class SaveStateHandler {
+    private Logger logger = Logger.getLogger(SaveStateHandler.class.getName());
+
+    public void writeState(File saveFile, SaveState state) throws IOException {
+        if (!saveFile.exists()) saveFile.createNewFile();
+        if (saveFile.canWrite()) {
+            try (FileWriter fileWriter = new FileWriter(saveFile)) {
+                fileWriter.write(state.toString());
+                fileWriter.close();
+            } catch (IOException e) {
+                /* TODO: Inform user */
+            }
+        }
+    }
+
+    public SaveState readState(File loadFile) throws FailedReadingStateException {
+        String readState;
+
+        if(loadFile.exists()){
+            try(LineNumberReader fileReader = new LineNumberReader(new FileReader(loadFile))){
+                readState = readFile(fileReader);
+            } catch (IOException e) {
+                logger.severe(e.getMessage());
+                throw new FailedReadingStateException(loadFile);
+            }
+        }else throw new FailedReadingStateException(loadFile);
+
+        return SaveState.createSaveState(readState);
+    }
+
+    private String readFile(LineNumberReader fileReader) throws IOException {
+        StringBuilder readState = new StringBuilder();
+        String readLine = fileReader.readLine();
+
+        while(readLine != null){
+            readState.append(readLine).append("\n");
+            readLine = fileReader.readLine();
+        }
+
+        return readState.toString();
+    }
+}
