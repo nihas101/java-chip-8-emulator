9cba15823ebb85ec4c7dddc2576cc4e6750b0ab7
Nikita Hasert <nikitahasert@gmail.com>
2018-02-11 15:55:34 +0100

    Create EmulatorKey class

---
+++ src/main/java/de/nihas101/chip8/hardware/keys/EmulatorKey.java
@@ -0,0 +1,23 @@
+package de.nihas101.chip8.hardware.keys;
+
+public class EmulatorKey {
+    private final String keyName;
+    private final EmulatorKeyEvent emulatorKeyEvent;
+
+    private EmulatorKey(String keyName, EmulatorKeyEvent emulatorKeyEvent) {
+        this.keyName = keyName;
+        this.emulatorKeyEvent = emulatorKeyEvent;
+    }
+
+    public static EmulatorKey createEmulatorKey(String keyName, EmulatorKeyEvent emulatorKeyEvent){
+        return new EmulatorKey(keyName, emulatorKeyEvent);
+    }
+
+    public String getKeyName() {
+        return keyName;
+    }
+
+    public void trigger(){
+        emulatorKeyEvent.trigger();
+    }
+}
